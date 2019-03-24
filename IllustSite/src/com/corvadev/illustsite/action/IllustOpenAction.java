4a2f36ed61e2953e75f7f5068ea1b3996ea950c4
otake-natsumi <iris1207l@yahoo.co.jp>
2019-03-24 23:18:23 +0900

    2019/03/24

--- IllustSite/src/com/corvadev/illustsite/action/IllustOpenAction.java
+++ IllustSite/src/com/corvadev/illustsite/action/IllustOpenAction.java
@@ -4,6 +4,23 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class IllustOpenAction extends ActionSupport{
 
+
+	<s:iterator value="productInfoDTOList">
+	<div class="product-list-box">
+	<ul>
+		<li>
+		<a href='<s:url action="ProductDetailsAction">
+		<s:param name="productId" value="%{productId}"/>
+		</s:url>'><img src='<s:property value="imageFilePath"/>/<s:property value="imageFileName"/>' class="item-image-box-200"/></a><br>
+		<s:property value="productName"/><br>
+		<s:property value="productNameKana"/><br>
+		<s:property value="price"/>円<br>
+	  </li>
+	</ul>
+	</div>
+	</s:iterator>
+
+
 	private String title;
 
 	public String getTitle(){
